87b1173edd87f312acd08d6c4d6ef032fd77b8a2
Fabien DUMINY <fduminy@jnode.org>
2013-09-07 15:29:27 +0200

    create and use a named TypeMapper for the archiveFactory property

---
+++ swing/src/main/java/fr/duminy/jbackup/swing/ArchiveFactoryTypeMapper.java
@@ -0,0 +1,89 @@
+/**
+ * JBackup is a software managing backups.
+ *
+ * Copyright (C) 2013-2013 Fabien DUMINY (fabien [dot] duminy [at] webmails [dot] com)
+ *
+ * JBackup is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * JBackup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+package fr.duminy.jbackup.swing;
+
+import fr.duminy.jbackup.core.archive.ArchiveFactory;
+import org.formbuilder.mapping.change.ChangeHandler;
+import org.formbuilder.mapping.typemapper.impl.ReferenceToComboboxMapper;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * A {@link org.formbuilder.TypeMapper} for {@link fr.duminy.jbackup.core.archive.ArchiveFactory}.
+ */
+public class ArchiveFactoryTypeMapper extends ReferenceToComboboxMapper<ArchiveFactory> {
+    private final List<ArchiveFactory> sortedFactories;
+
+    public ArchiveFactoryTypeMapper(ArchiveFactory[] factories) {
+        sortedFactories = new ArrayList<>(factories.length + 1);
+        for (ArchiveFactory f : factories) {
+            sortedFactories.add(f);
+        }
+        Collections.sort(sortedFactories, new Comparator<ArchiveFactory>() {
+            @Override
+            public int compare(ArchiveFactory o1, ArchiveFactory o2) {
+                return o1.getExtension().compareTo(o2.getExtension());
+            }
+        });
+        sortedFactories.add(0, null);
+
+    }
+
+    @Nonnull
+    @Override
+    public JComboBox createEditorComponent() {
+        JComboBox result = super.createEditorComponent();
+        result.setRenderer(ArchiveFactoryRenderer.INSTANCE);
+        return result;
+    }
+
+    @Nonnull
+    @Override
+    protected Collection getSuitableData() {
+        return sortedFactories;
+    }
+
+    @Override
+    public void handleChanges(@Nonnull JComboBox jComponent, @Nonnull ChangeHandler changeHandler) {
+        //TODO implement this.
+    }
+
+    @Nullable
+    @Override
+    public ArchiveFactory getValue(@Nonnull JComboBox jComponent) {
+        Object item = jComponent.getSelectedItem();
+        return (item instanceof ArchiveFactory) ? (ArchiveFactory) item : null;
+    }
+
+    @Nonnull
+    @Override
+    public Class getValueClass() {
+        return ArchiveFactory.class;
+    }
+
+    @Override
+    public void setValue(@Nonnull JComboBox jComponent, @Nullable ArchiveFactory o) {
+        jComponent.setSelectedItem(o);
+    }
+}
--- swing/src/main/java/fr/duminy/jbackup/swing/BackupConfigurationPanel.java
+++ swing/src/main/java/fr/duminy/jbackup/swing/BackupConfigurationPanel.java
@@ -28,13 +28,12 @@ import org.formbuilder.Form;
 import org.formbuilder.FormBuilder;
 import org.formbuilder.TypeMapper;
 import org.formbuilder.mapping.change.ChangeHandler;
-import org.formbuilder.mapping.typemapper.impl.ReferenceToComboboxMapper;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static fr.duminy.jbackup.core.BackupConfiguration.Source;
@@ -48,18 +47,6 @@ public class BackupConfigurationPanel extends JPanel {
     private final Form<BackupConfiguration> form;
 
     public BackupConfigurationPanel(ArchiveFactory... factories) {
-        final List<ArchiveFactory> sortedFactories = new ArrayList<>(factories.length + 1);
-        for (ArchiveFactory f : factories) {
-            sortedFactories.add(f);
-        }
-        Collections.sort(sortedFactories, new Comparator<ArchiveFactory>() {
-            @Override
-            public int compare(ArchiveFactory o1, ArchiveFactory o2) {
-                return o1.getExtension().compareTo(o2.getExtension());
-            }
-        });
-        sortedFactories.add(0, null);
-
         FormBuilder<BackupConfiguration> builder = map(BackupConfiguration.class).formsOf(REPLICATING);
 
         builder.useForProperty("sources", new TypeMapper<ListPanel<JList<Source>, Source>, Object>() {
@@ -122,44 +109,7 @@ public class BackupConfigurationPanel extends JPanel {
                 JList<Source> list = listPanel.getListComponent();
                 return (DefaultListModel<Source>) list.getModel();
             }
-        }).useForProperty("archiveFactory", new ReferenceToComboboxMapper() {
-            @Nonnull
-            @Override
-            public JComboBox createEditorComponent() {
-                JComboBox result = super.createEditorComponent();
-                result.setRenderer(ArchiveFactoryRenderer.INSTANCE);
-                return result;
-            }
-
-            @Nonnull
-            @Override
-            protected Collection getSuitableData() {
-                return sortedFactories;
-            }
-
-            @Override
-            public void handleChanges(@Nonnull JComponent jComponent, @Nonnull ChangeHandler changeHandler) {
-                //TODO implement this.
-            }
-
-            @Nullable
-            @Override
-            public Object getValue(@Nonnull JComponent jComponent) {
-                Object item = ((JComboBox) jComponent).getSelectedItem();
-                return (item instanceof ArchiveFactory) ? (ArchiveFactory) item : null;
-            }
-
-            @Nonnull
-            @Override
-            public Class getValueClass() {
-                return ArchiveFactory.class;
-            }
-
-            @Override
-            public void setValue(@Nonnull JComponent jComponent, @Nullable Object o) {
-                ((JComboBox) jComponent).setSelectedItem((ArchiveFactory) o);
-            }
-        });
+        }).useForProperty("archiveFactory", new ArchiveFactoryTypeMapper(factories));
 
         form = builder.buildForm();
 
